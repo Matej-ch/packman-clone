a9826fbce5d06585bcd03b1e6df444acdbbc3e3b
Matej <matejchalachan@gmail.com>
2022-10-16 19:27:08 +0200

    final update

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-import "../css/normalize.css"
-import "../css/main.css"
-
 document.addEventListener('DOMContentLoaded',() => {
     const grid = document.querySelector('.grid');
     const scoreDisplay = document.querySelector('.score');
